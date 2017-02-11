b54a224697fbb6696eab5e4a45092da4f8460017
Sean Hirata <seanchirata@gmail.com>
2017-02-11 16:44:10 -0500

    first models

--- chores/models.py
+++ chores/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Chore(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.IntegerField()
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Event(models.Model):
+    chore = models.ForeignKey(Chore)
+    person = models.ForeignKey(Person)
+    date = models.DateTimeField('date done')
